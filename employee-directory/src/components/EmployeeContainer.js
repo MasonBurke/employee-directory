85ffff5aeb0330699217d2ce605009c4e614f608
Mason Burke <masonburke@rocketmail.com>
2020-05-21 08:34:43 -0600

    started

---
+++ employee-directory/src/components/EmployeeContainer.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import Container from "./Container";
+import Row from "./Row";
+import Col from "./Col";
+import Card from "./Card";
+import SearchForm from "./SearchForm";
+import EmployeeDetail from "./EmployeeDetail";
+import API from "../utils/API";
+
+class EmployeeContainer extends Component {
+  state = {
+    result: {},
+    search: ""
+  };
+
+  // When this component mounts, search for the movie "The Matrix"
+  componentDidMount() {
+    this.searchEmployee();
+  }
+
+  searchEmployee = query => {
+    API.search(query)
+      .then(res => this.setState({ result: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, search the OMDB API for the value of `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchEmployee(this.state.search);
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col size="md-8">
+            <Card
+              heading={this.state.result.Title || "Search Employee"}
+            >
+              {this.state.result.Title ? (
+                <EmployeeDetail
+                  title={this.state.result.name}
+                  src={this.state.result.picture}
+                  email={this.state.result.email}
+
+                />
+              ) : (
+                <h3>No Results to Display</h3>
+              )}
+            </Card>
+          </Col>
+          <Col size="md-4">
+            <Card heading="Search">
+              <SearchForm
+                value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+              />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default EmployeeContainer;
